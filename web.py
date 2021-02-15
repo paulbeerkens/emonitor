f7a90dacbb2e8f5cbc388bbd7070fbb0c66c7b02
Paul Beerkens <paulbeerkens@gmail.com>
2021-02-15 13:56:27 -0600

    Show timestamp

--- web.py
+++ web.py
@@ -12,13 +12,16 @@ app=Flask(__name__)
 def current():
   current_power=db.load_latest()
   rows=[]
+  max_time=None
   for circuit in current_power:
     row_vals={}
     row_vals['id']=circuit.get('id',0)
     row_vals['name']=circuit.get('name','NA')
     row_vals['power']=circuit.get('power',0)
+    if max_time is None or circuit['time']>max_time:
+      max_time=circuit['time']
     rows.append(row_vals)
-  return render_template('/current.html',data=rows)
+  return render_template('/current.html',data=rows,time=str(max_time))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
